a472cab6fb276dc21a7b5fe3d55c06bb90d27624
yangmengyun <ymy513@gmail.com>
2015-03-04 09:35:20 +0800

    It is not satisfied...

--- 0006/CountWords2.py
+++ 0006/CountWords2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 import os,sys
-import operator
 from collections import Counter
 
 
 for txtfile in os.listdir(sys.argv[1]):
     txt=open(sys.argv[1]+"/"+txtfile)
     wordcount=Counter(txt.read().split())
-    
     print [key for key, val in wordcount.iteritems() if val == max (wordcount.values())] 
