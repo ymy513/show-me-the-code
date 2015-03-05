c543be4c6dac08da7251ad166fc82672a1b8ca83
yangmengyun <ymy513@gmail.com>
2015-03-05 16:49:06 +0800

    Resize images

--- 0005/Resize.py
+++ 0005/Resize.py
@@ -7,9 +7,7 @@ size=1136,640
 for infile in os.listdir(sys.argv[1]):
     outfile = os.path.splitext(infile)[0]+".thumbnail"
     if infile!=outfile:
-        try:
-            im=Image.open(sys.argv[1]+"/"+infile)
-            im.thumbnail(size,Image.ANTIALIAS)
-            im.save(sys.argv[1]+"/"+outfile,"JPEG")
-        except IOError:
-            print "cannot create thumnail for '%s'" % infile
+        im=Image.open(os.path.join(sys.argv[1],infile))
+        im.thumbnail(size,Image.ANTIALIAS)
+        im.save(os.path.join(sys.argv[1],outfile),"JPEG")
+        
