0adff147cd079864a97748780f81826937713143
yangmengyun <ymy513@gmail.com>
2015-03-15 12:16:43 +0800

    txt to xls

---
+++ 0015/txt2xls.py
@@ -0,0 +1,28 @@
+#! usr/bin/env python
+
+import json
+import xlwt
+import sys
+reload(sys)
+
+sys.setdefaultencoding="utf-8"
+def txt2xls(filename,xlsname):
+
+    wb = xlwt.Workbook(encoding="utf-8")
+    ws = wb.add_sheet("sheet1")
+
+    txt = open(filename).read()
+    json_txt = json.loads(txt)
+    row = 0
+    col = 0
+    for k,v in json_txt.iteritems():
+        ws.write(row,col,k)
+        ws.write(row,col+1,v)
+        row += 1
+
+    wb.save(xlsname+'.xls')
+
+if __name__ =='__main__':
+    filename = sys.argv[1]
+    xlsname = sys.argv[2]
+    txt2xls(filename,xlsname)
