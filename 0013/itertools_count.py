0c390335288f2e7863e44b502d8450836d48a020
yangmengyun <ymy513@gmail.com>
2015-03-17 18:07:00 +0800

    itertoos.count

---
+++ 0013/itertools_count.py
@@ -0,0 +1,11 @@
+# encoding=utf-8
+class itertoos(object):
+    # count(10)-->10,11,12,13,……
+    # count(2.5,0.5)-->2.5,3,3.5,4,……
+    def count(self,start,step):    
+        n = start
+        while True:
+            yield n
+            n +=step
+
+    # start + step * i for i in count()
