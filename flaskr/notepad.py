818ba6e4851c6541802a091c6110b477536dfe3c
yangmengyun <ymy513@gmail.com>
2015-03-23 14:53:55 +0800

    fix bugs

--- flaskr/notepad.py
+++ flaskr/notepad.py
@@ -1,3 +1,4 @@
+#! usr/bin/env python
 # encoding=utf-8
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
-
+c.execute('create table notes (id text, note text, time text)')
 
 @app.route('/')
 def show_notes(): 
@@ -17,7 +18,7 @@ def show_notes():
     notes = [dict(title=row[0], text=row[1], time=row[2]) for row in cur.fetchall()]
     return render_template('show_notes.html', notes=notes)
 
-@app.route('/add',methods=['POST'])
+@app.route('/add',methods=['GET','POST'])
 def add_note():
     c.execute("insert into notes (id,note,time) values (?,?,?)",
                 [request.form['id'],request.form['note'],datetime()])
